5d977b8dba5d28f65508acfcb4204ac022d44335
AntonioMora <126864866+Antoniomorales17@users.noreply.github.com>
2023-06-28 09:08:36 +0000

    Diagrama

--- src/models.py
+++ src/models.py
@@ -7,23 +7,52 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'User'
+  
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    surname = Column (String(250), nullable=False)
+    email = Column(String(90), unique=True, nullable=False)
+    phone_number = Column(String(20), unique=True, nullable=False)
+    favoritos = relationship('Favoritos',backref='personajes', lazy=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Personajes(Base):
+    __tablename__ = 'personajes'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    height = Column(String(250), nullable=False)
+    gender =  Column(String(250), nullable=False)
+    hair_color =  Column(String(250), nullable=False)
+    Favoritos= relationship("favoritos", backref="personajes", lazy=True)
+
+class Planetas(Base):
+    __tablename__ = 'planetas'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    diameter = Column(String(250), nullable=False)
+    orbital_period =  Column(String(250), nullable=False)
+    population =  Column(String(250), nullable=False)
+    Favoritos = relationship("favoritos", backref="planetas", lazy=True)
+
+class Vehiculos(Base):
+    __tablename__ = 'vehiculos'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    cargo_capacity =  Column(String(250), nullable=False)
+    length =  Column(String(250), nullable=False)
+    model =  Column(String(250), nullable=False)
+    favoritos= relationship("favoritos", backref="vehiculos", lazy=True)
+
+class Favoritos(Base):
+    __tablename__ = 'favoritos'
+    id = Column(Integer, primary_key=True)
+    usuario_id =Column(Integer, ForeignKey('usuario.id'))
+    planetas_id = Column(Integer, ForeignKey('planetas.id'))
+    personajes_id = Column(Integer, ForeignKey('personajes.id'))
+    Vehiculos_id = Column(Integer, ForeignKey("vehiculos.id"))
+
+
 
     def to_dict(self):
         return {}
